4f5bfb4c045c43727d6edcbc7227fe689e27d42a
Federico Cerutti <federico@ceres-c.it>
2025-10-14 23:24:58 +0200

    Quick preview with system default

--- MacPacker/Views/WelcomeView.swift
+++ MacPacker/Views/WelcomeView.swift
@@ -68,8 +68,9 @@ struct WhatsNewPill: View {
 struct WelcomeWhatsNewView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
-            WhatsNewPill(title: "Re-enable macOS 13 as minimum deployment target", type: .core)
+            WhatsNewPill(title: "Use system preview instead of internal previewer", type: .feature)
             WhatsNewPill(title: "Crash when using the open with option", type: .bug)
+            WhatsNewPill(title: "Re-enable macOS 13 as minimum deployment target", type: .core)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .padding()
