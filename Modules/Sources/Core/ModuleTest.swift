31486458caaf6a715c6342cbc3358355593f06ec
Stephan Arenswald <stephan@sarensw.com>
2025-11-17 13:50:13 +0100

    core: New target to hold the core logic in the new spm package

---
+++ Modules/Sources/Core/ModuleTest.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class ModuleTest {
+    public init() {}
+    
+    public func printHello() {
+        print("Hello, World!")
+    }
+}
