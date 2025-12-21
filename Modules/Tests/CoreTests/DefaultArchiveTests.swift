e8391024448b12c6ed24a4a28e4e10306f2c1c68
Stephan Arenswald <stephan@sarensw.com>
2025-12-21 14:26:01 +0100

    feat: more formats

--- Modules/Tests/CoreTests/DefaultArchiveTests.swift
+++ Modules/Tests/CoreTests/DefaultArchiveTests.swift
@@ -35,38 +35,44 @@ import Testing
     
     @Test("Test all defaultArchives on 7zip", arguments: [
         // ext, id, folder entries
-        ("7z", "7zip", true),
-        ("arj", "arj", false),
-        ("cab", "cab", false),
-        ("cpio", "cpio", true),
-        ("lzh", "lha", false),
-        ("rar", "rar", true),
-        ("tar", "tar", true),
-        ("zip", "zip", true),
+        ("7z", "7zip", 4, 2),
+        ("arj", "arj", 3, 2),
+        ("ar", "ar", 3, 3), // `ar` archives do not store folders, only folder paths
+        ("cab", "cab", 3, 2),
+        ("cpio", "cpio", 4, 2),
+        ("lzh", "lha", 3, 2),
+        ("rar", "rar", 4, 2),
+        ("tar", "tar", 4, 2),
+        ("xar", "xar", 5, 3), // additional `[TOC].xml`
+        ("zip", "zip", 4, 2),
+        // installers
+        ("rpm", "rpm", 1, 1),
         // disk images
-        ("dmg", "dmg", true),
-        ("iso", "iso", true),
-        ("qcow2", "qcow2", true),
-        ("squashfs", "squashfs", true),
-        ("vdi", "vdi", true),
-        ("vhd", "vhd", true),
-        ("vhdx", "vhdx", true),
-        ("vmdk", "vmdk", true),
+        ("dmg", "dmg", 4, 2),
+        ("fat", "fat", 4, 2),
+        ("iso", "iso", 4, 2),
+        ("qcow2", "qcow2", 4, 2),
+        ("squashfs", "squashfs", 4, 2),
+        ("vdi", "vdi", 4, 2),
+        ("vhd", "vhd", 4, 2),
+        ("vhdx", "vhdx", 4, 2),
+        ("vmdk", "vmdk", 4, 2),
         // compounds
-        ("tar.bz2", "tar", true),
-        ("tar.gz", "tar", true),
-        ("tar.xz", "tar", true),
-        ("tar.Z", "tar", true),
+        ("tar.bz2", "tar", 4, 2),
+        ("tar.gz", "tar", 4, 2),
+        ("tar.xz", "tar", 4, 2),
+        ("tar.Z", "tar", 4, 2),
         // comounds with one ending
-        ("tbz2", "tar", true),
-        ("tgz", "tar", true),
-        ("txz", "tar", true),
-        ("taz", "tar", true)
+        ("tbz2", "tar", 4, 2),
+        ("tgz", "tar", 4, 2),
+        ("txz", "tar", 4, 2),
+        ("taz", "tar", 4, 2)
     ])
-    func testAll7zipEngine(arg: (String, String, Bool)) async throws {
+    func testAll7zipEngine(arg: (String, String, Int, Int)) async throws {
         let ext = arg.0
         let id = arg.1
-        let folderEntries = arg.2
+        let entries = arg.2
+        let rootChildren = arg.3
         let controller = ArchiveState(catalog: ArchiveTypeCatalog(), engineSelector: ArchiveEngineSelector7zip())
         let folderURL = Bundle.module.url(forResource: "defaultArchives", withExtension: nil)!
         
@@ -76,8 +82,8 @@ import Testing
         print("")
         
         #expect(controller.type?.id == id)
-        #expect(controller.entries.count == (folderEntries ? 4 : 3))
-        #expect(controller.root?.children?.count == 2)
+        #expect(controller.entries.count == entries)
+        #expect(controller.root?.children?.count == rootChildren)
         #expect(controller.root === controller.selectedItem)
     }
     
@@ -104,39 +110,6 @@ import Testing
         #expect(controller.type?.id == id)
         #expect(controller.entries.count == (folderEntries ? 4 : 3))
         #expect(controller.root?.children?.count == 2)
-        #expect(controller.root === controller.selectedItem)
-    }
-    
-    @Test("Test rpm defaultArchive on 7zip")
-    func testRpm7zipEngine() async throws {
-        let controller = ArchiveState(catalog: ArchiveTypeCatalog(), engineSelector: ArchiveEngineSelector7zip())
-        let folderURL = Bundle.module.url(forResource: "defaultArchives", withExtension: nil)!
-        
-        let url = folderURL.appendingPathComponent("defaultArchive.\("rpm")")
-        
-        try await controller.openAsync(url: url)
-        print("")
-        
-        #expect(controller.type?.id == "rpm")
-        #expect(controller.entries.count == 1)
-        #expect(controller.root?.children?.count == 1)
-        #expect(controller.root === controller.selectedItem)
-    }
-    
-    /// `ar` archives do not store folders, only folder paths
-    @Test("Test ar defaultArchive on 7zip")
-    func testAr7zipEngine() async throws {
-        let controller = ArchiveState(catalog: ArchiveTypeCatalog(), engineSelector: ArchiveEngineSelector7zip())
-        let folderURL = Bundle.module.url(forResource: "defaultArchives", withExtension: nil)!
-        
-        let url = folderURL.appendingPathComponent("defaultArchive.\("ar")")
-        
-        try await controller.openAsync(url: url)
-        print("")
-        
-        #expect(controller.type?.id == "ar")
-        #expect(controller.entries.count == 3)
-        #expect(controller.root?.children?.count == 3)
         #expect(controller.root === controller.selectedItem)
     }
 }
